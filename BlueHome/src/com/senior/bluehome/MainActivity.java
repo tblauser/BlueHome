97dcef599a74c256ce3a98e15262fc9fbca1cb1b
tblauser <treyblauser@yahoo.com>
2013-11-20 21:15:41 -0600

    Bluetooth compatibility check added. Also asks to turn on Bluetooth when app is opened up if Bluetooth isn't already on

--- BlueHome/src/com/senior/bluehome/MainActivity.java
+++ BlueHome/src/com/senior/bluehome/MainActivity.java
@@ -1,15 +1,49 @@
 package com.senior.bluehome;
 
+
+
+
+
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
 import android.view.Menu;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+	
+	private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+	// Name of connected device
+    private String ConnectedDeviceName = null;
+    // Local Bluetooth adapter
+    private BluetoothAdapter BluetoothAdapter = null;
+    // Member object for Bluetooth Command Service
+   // private BluetoothCommandService CommandService = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		BluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
+		
+		if(BluetoothAdapter == null){
+			Toast.makeText(this, "Bluetooth not available on this device", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+		}
+		
 	}
 
 	@Override
@@ -18,5 +52,30 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	@Override
+	protected void onStart() {
+		super.onStart();
+		
+		// If Bluetooth not on then request enable.
+        // setupCommand() will then be called during onActivityResult
+		if (!BluetoothAdapter.isEnabled()) {
+			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+		}
+		// otherwise set up the command service
+		else {
+		//	if (CommandService==null)
+				setupCommand();
+		}
+	}
+	
+	
+	
+	private void setupCommand() {
+		// Initialize the BluetoothChatService to perform bluetooth connections
+      //  CommandService = new BluetoothCommandService(this, Handler);
+	}
+	
 
 }

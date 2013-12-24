4e0d2e3fdccb6a281727b43b9d20271fdc88ed7f
Shashank <Shashank@shashanks-mbp.home>
2013-12-24 16:19:01 -0600

    Completed on click listener and menu action bar.

--- BlueHome/src/com/senior/bluehome/MainActivity.java
+++ BlueHome/src/com/senior/bluehome/MainActivity.java
@@ -27,6 +27,7 @@ import android.widget.ToggleButton;
 public class MainActivity extends Activity {
 	private ToggleButton toggle_living_room, toggle_kitchen,toggle_master_bedroom;
 	private ToggleButton toggle_bedroom1,toggle_bedroom2;
+	
 	private Button btnDisplay;
 	private static final int REQUEST_CONNECT_DEVICE = 1;
     private static final int REQUEST_ENABLE_BT = 2;
@@ -50,17 +51,67 @@ public class MainActivity extends Activity {
 //            return;
 //		}
 		
-		addListenerOnButton();
+		addListenerOnLightButton();
 		 
 	  }
 	 
-	  public void addListenerOnButton() {
+	  public void addListenerOnLightButton() {
 	 
 		toggle_living_room = (ToggleButton) findViewById(R.id.toggle_living_room);
+		toggle_living_room.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Living Room : ").append(toggle_living_room.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
 		toggle_kitchen = (ToggleButton) findViewById(R.id.	toggle_kitchen);
+		toggle_kitchen.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Kitchen : ").append(toggle_kitchen.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
 		toggle_master_bedroom = (ToggleButton) findViewById(R.id.toggle_master_bedroom);
+		toggle_master_bedroom.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Master Bedroom : ").append(toggle_master_bedroom.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
 		toggle_bedroom1 = (ToggleButton) findViewById(R.id.toggle_bedroom1);
+		toggle_bedroom1.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Bedroom 1 : ").append(toggle_bedroom1.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
 		toggle_bedroom2 = (ToggleButton) findViewById(R.id.toggle_bedroom2);
+		toggle_bedroom2.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Bedroom 2 : ").append(toggle_bedroom2.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
 		btnDisplay = (Button) findViewById(R.id.btnDisplay);
 	 
 		btnDisplay.setOnClickListener(new OnClickListener() {

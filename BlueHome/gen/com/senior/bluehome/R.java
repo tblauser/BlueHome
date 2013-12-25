764848b75096ecf2b9ca55395f06f19798a9eba0
tblauser <treyblauser@yahoo.com>
2013-12-24 23:42:22 -0600

    Had some conflicts pulling Shashank's changes, had to checkout his code and reset it as my branch then commit the branch to master.

--- BlueHome/gen/com/senior/bluehome/R.java
+++ BlueHome/gen/com/senior/bluehome/R.java
@@ -24,19 +24,44 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int textView1=0x7f080000;
+        public static final int action_settings=0x7f08000e;
+        public static final int btnDisplay=0x7f08000a;
+        public static final int home_controls=0x7f08000c;
+        public static final int media_player=0x7f08000d;
+        public static final int textView1=0x7f08000b;
+        public static final int text_bedroom1=0x7f080003;
+        public static final int text_bedroom2=0x7f080004;
+        public static final int text_kitchen=0x7f080001;
+        public static final int text_living_room=0x7f080000;
+        public static final int text_master_bedroom=0x7f080002;
+        public static final int toggle_bedroom1=0x7f080008;
+        public static final int toggle_bedroom2=0x7f080009;
+        public static final int toggle_kitchen=0x7f080006;
+        public static final int toggle_living_room=0x7f080005;
+        public static final int toggle_master_bedroom=0x7f080007;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int media_player=0x7f030001;
+        public static final int settings=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_settings=0x7f050003;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int bedroom1=0x7f050009;
+        public static final int bedroom2=0x7f050006;
+        public static final int btn_display=0x7f05000c;
+        public static final int hello_world=0x7f050004;
+        public static final int home_controls=0x7f050001;
+        public static final int kitchen=0x7f050007;
+        public static final int living_room=0x7f050005;
+        public static final int master_bedroom=0x7f050008;
+        public static final int media_player=0x7f050002;
+        public static final int toggle_off=0x7f05000b;
+        public static final int toggle_on=0x7f05000a;
     }
     public static final class style {
         /** 
--- BlueHome/src/com/senior/bluehome/MainActivity.java
+++ BlueHome/src/com/senior/bluehome/MainActivity.java
@@ -1,10 +1,5 @@
 package com.senior.bluehome;
 
-
-
-
-
-
 import android.os.Bundle;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -16,12 +11,19 @@ import android.os.Message;
 import android.view.KeyEvent;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends Activity {
+	private ToggleButton toggle_living_room, toggle_kitchen,toggle_master_bedroom;
+	private ToggleButton toggle_bedroom1,toggle_bedroom2;
 	
+	private Button btnDisplay;
 	private static final int REQUEST_CONNECT_DEVICE = 1;
     private static final int REQUEST_ENABLE_BT = 2;
 	// Name of connected device
@@ -36,15 +38,113 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		BluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
+//		BluetoothAdapter = android.bluetooth.BluetoothAdapter.getDefaultAdapter();
+//		
+//		if(BluetoothAdapter == null){
+//			Toast.makeText(this, "Bluetooth not available on this device", Toast.LENGTH_LONG).show();
+//            finish();
+//            return;
+//		}
 		
-		if(BluetoothAdapter == null){
-			Toast.makeText(this, "Bluetooth not available on this device", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-		}
-		
+		addListenerOnLightButton();
+		 
+	  }
+	 
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.home_controls:
+	        	return super.onOptionsItemSelected(item);
+	        case R.id.media_player:
+	            startActivity(new Intent(this, MediaPlayer.class));
+	            return true;
+	        case R.id.action_settings:
+	            startActivity(new Intent(this, Settings.class));
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
 	}
+
+	public void addListenerOnLightButton() {
+	 
+		toggle_living_room = (ToggleButton) findViewById(R.id.toggle_living_room);
+		toggle_living_room.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Living Room : ").append(toggle_living_room.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		toggle_kitchen = (ToggleButton) findViewById(R.id.	toggle_kitchen);
+		toggle_kitchen.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Kitchen : ").append(toggle_kitchen.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		toggle_master_bedroom = (ToggleButton) findViewById(R.id.toggle_master_bedroom);
+		toggle_master_bedroom.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Master Bedroom : ").append(toggle_master_bedroom.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		toggle_bedroom1 = (ToggleButton) findViewById(R.id.toggle_bedroom1);
+		toggle_bedroom1.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Bedroom 1 : ").append(toggle_bedroom1.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		toggle_bedroom2 = (ToggleButton) findViewById(R.id.toggle_bedroom2);
+		toggle_bedroom2.setOnClickListener(new OnClickListener() {		 
+			@Override
+			public void onClick(View v) {
+			   StringBuffer result = new StringBuffer();
+			   result.append("Bedroom 2 : ").append(toggle_bedroom2.getText());
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		btnDisplay = (Button) findViewById(R.id.btnDisplay);
+	 
+		btnDisplay.setOnClickListener(new OnClickListener() {
+	 
+			@Override
+			public void onClick(View v) {
+	 
+			   StringBuffer result = new StringBuffer();
+			   result.append("Living Room : ").append(toggle_living_room.getText());
+			   result.append("\nKitchen : ").append( toggle_kitchen.getText());
+			   result.append("\nMaster Bedroom : ").append(toggle_master_bedroom.getText());
+			   result.append("\nBedroom 1 : ").append(toggle_bedroom1.getText());
+			   result.append("\nBedroom 2 : ").append(toggle_bedroom2.getText());
+	 
+			   Toast.makeText(MainActivity.this, result.toString(),
+				Toast.LENGTH_SHORT).show();
+	 
+			}
+	 
+		});
+	  }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -57,17 +157,17 @@ public class MainActivity extends Activity {
 	protected void onStart() {
 		super.onStart();
 		
-		// If Bluetooth not on then request enable.
-        // setupCommand() will then be called during onActivityResult
-		if (!BluetoothAdapter.isEnabled()) {
-			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
-		}
-		// otherwise set up the command service
-		else {
-		//	if (CommandService==null)
-				setupCommand();
-		}
+//		// If Bluetooth not on then request enable.
+//        // setupCommand() will then be called during onActivityResult
+//		if (!BluetoothAdapter.isEnabled()) {
+//			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+//		}
+//		// otherwise set up the command service
+//		else {
+//		//	if (CommandService==null)
+//				setupCommand();
+//		}
 	}
 	
 	

2db0bf6f883b3b3dab94c47992a08774bcb9fc67
tblauser <treyblauser@yahoo.com>
2014-01-02 17:33:06 -0600

    Commit generated file changes so I can pull latest code changes

--- BlueHome/gen/com/senior/bluehome/R.java
+++ BlueHome/gen/com/senior/bluehome/R.java
@@ -29,6 +29,8 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int media_player=0x7f030001;
+        public static final int settings=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
